34556f38dedbda058adddae612902f1c2243efc9
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-31 17:15:27 -0300

    removing polyfill

--- src/index.js
+++ src/index.js
@@ -6,19 +6,6 @@ import {
 } from './injector';
 import processImage from './process_image';
 
-if (!Object.entries) {
-  Object.entries = function entries(obj) {
-    const ownProps = Object.keys(obj);
-    let i = ownProps.length;
-    const resArray = new Array(i); // preallocate the Array
-
-    while (i--) {
-      resArray[i] = [ownProps[i], obj[ownProps[i]]];
-    }
-    return resArray;
-  };
-}
-
 class SocialTagsPlugin {
   constructor(options = {}) {
     this.htmlPlugin = false;
